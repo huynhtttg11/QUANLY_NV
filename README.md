fbb0bdf1741fa3a3fcf3f62dfca367cd84f2afd5
Trung Huynh Thanh <hthanhtrung@tma.com.vn>
2019-05-29 10:06:47 +0700

    Asda test

--- README.md
+++
@@ -1,1 +0,0 @@
-# QUANLY_NV
--- wss-agent-api/src/main/java/org/whitesource/agent/api/AgentApiVersion.java
+++
@@ -1,9 +0,0 @@
-package org.whitesource.agent.api;
-
-/**
- * @author eRez Huberman
- **/
-public class AgentApiVersion {
-
-    public static final double AGENT_API_VERSION_2_9_8 = 2.98;
-}
